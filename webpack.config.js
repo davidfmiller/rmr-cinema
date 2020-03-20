85b54f7852f505b7b62167b09ae65f1a6a6f81d3
David Miller <david.miller@showpass.com>
2020-03-20 13:53:25 -0600

    0.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,9 @@ const config = {
 //  mode: 'production',
   watch: true,
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
-    })
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress: { warnings: false }
+//     })
   ],
   module: {
     rules: [
